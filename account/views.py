3d230d068f8a3c84af170334e11b80c0faa7b088
trolchiha <trotsko.olena@gmail.com>
2024-06-23 22:43:37 +0300

    Added search filter and ordering for accounts

--- account/views.py
+++ account/views.py
@@ -1,11 +1,18 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from account.models import Account
 from account.serializers import AccountSerializer
     
 class AccountAPIView(ListCreateAPIView):
     serializer_class = AccountSerializer
     permission_classes = (IsAuthenticated, )
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    filterset_fields = ('id', 'name', 'balance', 'currency')
+    search_fields = ('name', 'balance', 'currency')
+    ordering_fields = ('id', 'name', 'balance', 'currency')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
